e4cc76c978a61f42c5b070ff4f390fb6c90fd09c
Atilla-Main <info@roostercoop.ninja>
2022-03-28 12:23:30 -0400

    changes to install script.

--- bin/install.sh
+++ bin/install.sh
@@ -1,39 +1,39 @@
 # Add and Update packages
 add-apt-repository universe
 apt update
-apt upgrade
+apt upgrade -y
 
 ########################################################
 # Install Steam Components
 ########################################################
 apt install libappindicator1 libnm0 libtcmalloc-minimal4
 
-# Set up steam profile
-echo "Setting Steam Variables"
-echo "STEAMOS=1" >> /etc/profile.d/steam.sh
-echo "STEAM_RUNTIME=1" >> /etc/profile.d/steam.sh
-
-# Setup env for steam install
-echo "Exporting the Steam variables"
-export STEAMOS=1
-export STEAM_RUNTIME=1
-
 # installing steam client
 echo "Downloading and installing Steam Client"
-wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb
-dpkg -i steam.deb
+apt install -y steam 
 
 ########################################################
 # Install MAME Arcade Engine
 ########################################################
-# downloading from here: https://www.romspack.com/mame-roms-pack/
-apt install mame
-echo "Downloading mame.2411x.zip"
-wget https://github.com/mamedev/mame/releases/download/mame0241/mame0241lx.zip
+# downloading the packs I like from here: https://www.romspack.com/mame-roms-pack/ 
+echo "Installing MAME Arcade Engine and QMC2"
+apt install -y mame 
+
+add-apt-repository ppa:mmbossoni-gmail/emu
+apt update
+apt install qmc2-sdlmame qmc2-arcade qmc2-data qchdman
+apt update
+apt upgrade -y
+
+# Example of having to install specific rom repositories
+# echo "Installing NeoGeo Games"
+# wget https://archive.org/compress/neo-geo-aes-romset/formats=ZIP&file=/neo-geo-aes-romset.zip
+# echo "Unzipping the NeoGeo rompacks"
+# unzip neo-geo-aes-romset.zip -d /usr/local/share/games/mame/roms/
 
 # unzip downloaded pack
-echo "Unzipping the rompack"
-unzip mame0241lx.zip
+# echo "Unzipping the rompacks"
+# unzip
 
 ########################################################
 # Enable Auto Update weekly
---
+++ bin/setup.sh
@@ -0,0 +1,3 @@
+# Downloading Resources
+echo "Downloading Ubuntu 20.04 Desktop"
+wget https://releases.ubuntu.com/20.04.4/ubuntu-20.04.4-desktop-amd64.iso
